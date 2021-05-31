08e787a9a443c90b08d0e4631e6c143eba62bed4
qiujin123 <qiujinzou123@gmail.com>
2021-05-31 16:21:10 -0400

    added gmc logo

---
+++ dev/js/gmc.js
@@ -0,0 +1,10 @@
+import { gsap } from "gsap";
+
+export let gmcTL = gsap.timeline();
+
+
+gmcTL.from("#tower1",{ duration:0.6, scale:0.1, alpha:0, ease: "power2.out"},"tower")
+        
+        
+        
+            
