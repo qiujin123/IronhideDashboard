58cb854cc827d0017e797ecce4405a93be81eab0
qiujin123 <qiujinzou123@gmail.com>
2021-06-03 18:08:52 -0400

    first look of dashboard

--- dev/js/classicOne.js
+++ dev/js/classicOne.js
@@ -6,9 +6,26 @@ gsap.registerPlugin(MorphSVGPlugin);
 
 export let classicOneTL = gsap.timeline();
 
+// gsap.set("#leftHandleUpView",{transformOrigin:"center"});
+// gsap.set("#rightHandleUpView",{transformOrigin:"center"});
+
 
 classicOneTL.to("#autobotFull",{ duration:0.1, alpha: 0, delay:1}) 
             .to("#gmcMBroke",{ duration:0.1, alpha: 0})  
-            .to("#auto16", {duration:1, morphSVG:"#doorTransform", fill:"#5E5B64", y:-238.5, x:-393},"transform")
-            .to("#auto11", {duration:1, morphSVG:"#leftHandleUpBase", x:-200},"transform")
-            .to("#auto9", {duration:1, morphSVG:"#rightHandleUpBase", x:200},"transform")
+            .to("#auto7", {duration:1, alpha:0, y:50},"transform")
+            .to("#auto8", {duration:1, alpha:0, y:50},"transform")
+            .to("#auto10", {duration:1, alpha:0, y:50},"transform")
+            .to("#auto12", {duration:1, alpha:0, y:50},"transform")
+            .to("#auto15", {duration:1, alpha:0, y:50},"transform")
+            .to("#auto155", {duration:1, alpha:0, y:50},"transform")
+            .to("#auto1", {duration:1, alpha:0, y:50},"transform")
+            .to("#auto2", {duration:1, alpha:0, y:50},"transform")
+            .to("#auto3", {duration:1, alpha:0, y:50},"transform")
+            .to("#auto4", {duration:1, alpha:0, y:50},"transform")
+            .to("#auto5", {duration:1, alpha:0, y:50},"transform")
+            .to("#auto6", {duration:1, alpha:0, y:50},"transform")
+            .to("#auto13", {duration:1, alpha:0, y:50},"transform")
+            .to("#auto14", {duration:1, alpha:0, y:50},"transform")
+            .to("#auto16", {duration:1, morphSVG:"#doorTransform", fill:"#5E5B64", y:-179, x:-357},"transform")
+            .to("#auto11", {duration:1, morphSVG:"#leftHandleUpView", x:-355.5, y:-176},"transform")
+            .to("#auto9", {duration:1, morphSVG:"#rightHandleUpView", x:-357.5, y:-176},"transform")
