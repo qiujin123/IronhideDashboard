400f7710da86535481703d460f65638fd4ea3fe5
qiujin123 <qiujinzou123@gmail.com>
2021-06-09 21:06:39 -0400

    quick fixes

--- dev/js/speed.js
+++ dev/js/speed.js
@@ -1,21 +1,21 @@
 import { gsap } from "gsap";
 export let speedTL = gsap.timeline();
 
-speedTL.to("#skinMph1",{ duration: 0.5, fill:"#D34F47", onStart: startTimer})
+speedTL.to("#skinMph1",{ duration: 0.3, fill:"#D34F47", onStart: startTimer})
 	   .to("#rpmZero",{ duration: 0.3, alpha:0})	
-	   .to("#skinMph2",{ duration: 0.5, fill:"#CF4037"},"one")
+	   .to("#skinMph2",{ duration: 0.3, fill:"#CF4037"},"one")
 	   .from("#rpmOne",{ duration: 0.3, alpha:0 },"one")
-	   .to("#skinRpm1",{ duration: 0.5, fill:"#279DDC"},"one")
+	   .to("#skinRpm1",{ duration: 0.3, fill:"#279DDC"},"one")
 	   .to("#rpmOne",{ duration: 0.3, alpha:0})
-	   .to("#skinMph3",{ duration: 0.5, fill:"#C3382F"},"two")
+	   .to("#skinMph3",{ duration: 0.3, fill:"#C3382F"},"two")
 	   .from("#rpmTwo",{ duration: 0.3, alpha:0 },"two")
-	   .to("#skinRpm2",{ duration: 0.5, fill:"#2192CE"},"two")
+	   .to("#skinRpm2",{ duration: 0.3, fill:"#2192CE"},"two")
 	   .to("#rpmTwo",{ duration: 0.3, alpha:0})
-	   .to("#skinMph4",{ duration: 0.5, fill:"#B4332B"},"three")
+	   .to("#skinMph4",{ duration: 0.3, fill:"#B4332B"},"three")
 	   .from("#rpmThree",{ duration: 0.3, alpha:0 },"three")
-	   .to("#skinRpm3",{ duration: 0.5, fill:"#1F86BD"},"three")
-	   .to("#skinMph5",{ duration: 0.5, fill:"#A42F27"})
-	   .to("#skinMph6",{ duration: 0.5, fill:"#942A24"})
+	   .to("#skinRpm3",{ duration: 0.3, fill:"#1F86BD"},"three")
+	   .to("#skinMph5",{ duration: 0.3, fill:"#A42F27"})
+	   .to("#skinMph6",{ duration: 0.3, fill:"#942A24"})
 	   .to("#rpmZero",{ duration: 0.3, alpha:0})
 	   
 	   
